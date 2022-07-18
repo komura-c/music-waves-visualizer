23893b79c556fed3965d50cd2143d1a6d634e5ce
komura <affirman03139@outlook.jp>
2022-07-18 22:54:09 +0900

    fix: add corePath, exit

--- scripts/Ffmpeg.ts
+++ scripts/Ffmpeg.ts
@@ -1,13 +1,20 @@
 import { createFFmpeg } from "@ffmpeg/ffmpeg";
 
+const ffmpegCoreVersion = "0.10.0";
+const corePath = `https://unpkg.com/@ffmpeg/core@${ffmpegCoreVersion}/dist/ffmpeg-core.js`;
+
 export async function generateMp4Video(
   binaryData: Uint8Array,
   webmName: string,
   mp4Name: string
 ) {
-  const ffmpeg = createFFmpeg({ log: true });
+  const ffmpeg = createFFmpeg({ corePath, log: true });
   await ffmpeg.load();
   ffmpeg.FS("writeFile", webmName, binaryData);
   await ffmpeg.run("-i", webmName, "-vcodec", "copy", mp4Name);
-  return ffmpeg.FS("readFile", mp4Name);
+  const videoUint8Array = ffmpeg.FS("readFile", mp4Name);
+  try {
+    ffmpeg.exit();
+  } catch (error) {}
+  return videoUint8Array;
 }
