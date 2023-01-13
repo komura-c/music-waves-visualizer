2fbc32a3640b93634e8bcb98bf610f58af94c293
komura-c <affirman03139@outlook.jp>
2023-01-13 17:09:23 +0900

    fix: copyright_year

--- pages/index.tsx
+++ pages/index.tsx
@@ -320,7 +320,7 @@ const Home: NextPage = () => {
         handleClose={handleClose}
       ></CustomSnackbar>
 
-      <footer className={styles.footer}>&copy; 2021 komura-c</footer>
+      <footer className={styles.footer}>&copy; komura-c</footer>
     </>
   );
 };
